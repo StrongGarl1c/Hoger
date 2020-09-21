456f9ed1fd9c8d7c90e607d0b93b980993581aa8
StrongGarl1c <jarkkocs16@rambler.ru>
2020-09-21 21:54:46 +0300

    deploy now fix later

---
+++ index.js
@@ -0,0 +1,72 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://Jarko:78789893@cluster0.n3xtd.gcp.mongodb.net/Cluster0?retryWrites=true&w=majority"
+  
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+const express = require("express");
+const { response } = require("express");
+const app = express();
+const port = process.env.PORT || 27017;
+app.listen(port, () => console.log(`server is running on port ${port}`));
+app.use(express.static("public"));
+app.use(express.json());
+
+//connect to db
+async function run() {
+  await client.connect();
+  console.log("conected to database");
+  const database = client.db("treasure");
+  const collection = database.collection("players");
+  // get top 15
+  app.get("/get", function (req, res, opt) {
+    let arr = [];
+    const sort = { score: 1 };
+    const limit = 15;
+    const projection = { _id: 0, name: 1, score: 1 };
+    const cursor = collection.find().project(projection);
+    cursor.sort(sort);
+    cursor.limit(limit);
+    cursor.forEach(
+      (elem) => {
+        arr.push(elem);
+      },
+      () => {
+        res.json(arr);
+      }
+    );
+  });
+
+  // data handler
+  app.post("/api", function (req, res) {
+    let data = req.body;
+    // request
+    const options = { upsert: true };
+    let query = { name: data.name };
+    let update = {
+      $set: {
+        name: data.name,
+        score: data.score,
+      },
+    };
+    collection.updateOne(query, update, options);
+
+    //response
+    let arr = [];
+    const sort = { score: 1 };
+    const limit = 15;
+    const projection = { _id: 0, name: 1, score: 1 };
+    const cursor = collection.find().project(projection);
+    cursor.sort(sort);
+    cursor.limit(limit);
+    cursor.forEach(
+      (elem) => {
+        arr.push(elem);
+      },
+      () => {
+        res.json(arr);
+      }
+    );
+  });
+}
+run().catch((err) => {
+  console.error(err);
+});
---
+++ public/script.js
@@ -0,0 +1,140 @@
+let playerName = prompt("Введите ник");
+  if (!playerName) {
+    playerName = "Player 1";
+  }
+document.querySelector("h1").innerText = playerName + ", Найди Дробителя!";
+let scoreDiv = document.getElementById("scoreList");
+let innerGrid = document.getElementById("innerGrid");
+// Получить случайное число от 0 до size-1
+var getRandomNumber = function (size) {
+  return Math.floor(Math.random() * size);
+};
+
+// Вычислить расстояние от клика (event) до клада (target)
+var getDistance = function (event, target) {
+  var diffX = event.offsetX - target.x;
+  var diffY = event.offsetY - target.y;
+  return Math.sqrt(diffX * diffX + diffY * diffY);
+};
+
+// Получить для расстояния строку подсказки
+var getDistanceHint = function (distance) {
+  if (distance < 20) {
+    return "Логово Рагнароса";
+  } else if (distance < 40) {
+    return "Пригорает";
+  } else if (distance < 80) {
+    return "Тепло";
+  } else if (distance < 160) {
+    return "Прохладно";
+  } else if (distance < 320) {
+    return "Очень холодно";
+  } else {
+    return "Холодно как в нордсколе";
+  }
+};
+
+// Создаем переменные
+var width = 1002;
+var height = 650;
+var clicks = 0;
+// Случайная позиция клада
+var target = {
+  x: getRandomNumber(width),
+  y: getRandomNumber(height),
+};
+// Добавляем элементу img обработчик клика
+document.getElementById("map").addEventListener("click", function go(event) {
+  clicks++;
+  // Получаем расстояние от места клика до клада
+  var distance = getDistance(event, target);
+  // Преобразуем расстояние в подсказку
+  var distanceHint = getDistanceHint(distance);
+  // Записываем в элемент #distance новую подсказку
+  document.getElementById("distance").innerText = distanceHint;
+  // Если клик был достаточно близко, поздравляем с победой
+  if (distance < 25) {
+    alert(
+      "Дробитель найден! Сделано кликов: " +
+        clicks +
+        "\nНу ты индеец, БОМ, БОМ."
+    );
+    // добавити гравця в таблицю
+    // var paragraph = document.createElement("p");
+    // var total = playerName + " " + clicks + " " + numbers(clicks);
+    // scoreDiv.appendChild(paragraph).innerText = total;
+    document.getElementById("map").removeEventListener("click", go);
+
+    
+    // fetch
+    const data = {
+      name: playerName,
+      score: clicks
+    }
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    fetch('/api', options)
+    .then( async res => {
+      let data = await res.json();
+      data.forEach( elem => {
+        scoreDiv.remove();
+        let paragraph = document.createElement("p");
+        paragraph.innerText = elem.name + "   " + elem.score + " " + numbers(elem.score);
+        innerGrid.appendChild(paragraph);
+      })
+    });    
+  }
+});
+
+async function getData(){
+  const res = await fetch("/get");
+  const data = await res.json();
+  data.forEach( elem => {
+    let paragraph = document.createElement("p");
+    paragraph.innerText = elem.name + "   " + elem.score + " " + numbers(elem.score);
+    scoreDiv.appendChild(paragraph);
+  })
+}
+getData().catch( (err, wrn) => {
+   console.error(err);
+   console.warn(wrn);
+  });
+
+// назви кліків
+function numbers(clicks) {
+  switch (clicks) {
+    case 1:
+      return "клик";
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return "клика";
+      break;
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    case 20:
+      return "кликов";
+      break;
+    default:
+      return "много кликов :(";
+  }
+}
